f207288ab8c42f21916601f2c486417a294932f2
Bohdan <mylyibohdan18@gmail.com>
2023-10-26 21:51:52 +0300

    update all files

---
+++ app.js
@@ -0,0 +1,53 @@
+const xml = require("fast-xml-parser");
+const fs = require('fs');
+const http = require("http");
+
+
+
+const host = 'localhost';
+const portIsListen = 8000;
+
+
+
+const xmlFile = 'data.xml';
+const totalIncome = 'BS2_IncomeTotal';
+const totalExpenses = 'BS2_ExpensesTotal';
+
+
+const xmlData = fs.readFileSync(xmlFile, 'utf-8');
+const parser = new xml.XMLParser();
+const jsonData = parser.parse(xmlData);
+
+
+const finalDataXML = {
+    data: {
+      indicators: []
+    }
+  };
+  
+
+  for (let i = 0; i < jsonData.indicators.banksincexp.length; i++) {
+    const item = jsonData.indicators.banksincexp[i];
+    if (item.id_api === totalIncome || item.id_api === totalExpenses) {
+        filteredData.data.indicators.push({
+            txt: item.txt,
+            value: item.value
+        });
+    }
+}
+
+
+const builderForXML = new xml.XMLBuilder();
+const xmlStr = builderForXML.build(finalDataXML);
+
+
+const server = http.createServer(function(req,res){
+    res.setHeader('Content-type', 'text/xml');
+    res.setHeader('Access-Controll-Allow-Origin', "*");
+    res.writeHead(200); 
+    res.end(xmlStr);
+});
+
+server.listen(port,host, function(){
+    console.log(`Server on port ${portIsListen}`)
+})
